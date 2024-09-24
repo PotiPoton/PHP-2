941f6dcebc0a085bf35cc5c82698ef0659d37480
PotiPoton <leo.gilblas@sio-carriat.com>
2024-09-24 14:55:01 +0200

    modifs CSS

--- B2 PILLA/TP GSB/sources/include/form_fhf.php
+++ B2 PILLA/TP GSB/sources/include/form_fhf.php
@@ -6,7 +6,7 @@
     <h2>Saisie des frais hors forfait</h2>
     <form action="" method="POST">
         <input type="text" name="lib-fhf" placeholder="Entrer le libellé des frais">
-        <input type="number" name="mtn-fhf" placeholder="Etrer le montant des frais">
+        <input type="number" name="mtn-fhf" placeholder="Entrer le montant des frais">
         <input class="save" type="submit" name="btn-fhf" value="Enregistrer">
     </form>
 </div>
